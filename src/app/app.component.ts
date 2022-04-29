98010cb5d29e1fe74a022d6e04331abd28091710
Shubham Anand <60786471+22shubham22@users.noreply.github.com>
2022-04-29 19:39:19 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-starter';
   a=10;
   b=10;
+  constructor(public dialog : MatDialogRef<any>) {}
+
+  ngOnInit() {
+    this.foo();
+  }
+
+  foo() {
+    return 2+3;
+  }
 }
