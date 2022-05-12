2ee4d709c89461fa87061a4b7538436bc1662cbf
I526683 <shubham.anand@sap.com>
2022-05-12 11:11:42 +0530

    testing prettier rrc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,24 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'angular-starter';
-  a = 10;
-  b = 10;
-  constructor(public dialog: MatDialogRef<any>) {}
+	title = 'angular-starter';
+	a = 10;
+	b = 10;
+	constructor(public dialog: MatDialogRef<any>) {}
 
-  ngOnInit() {
-    this.foo();
-  }
+	ngOnInit() {
+		this.foo();
+	}
 
-  foo() {
-    const a = 5;
-    const b = 6;
-    var c = a + b;
-    return c;
-  }
+	foo() {
+		const a = 5;
+		const b = 6;
+		var c = a + b;
+		return c;
+	}
 }
