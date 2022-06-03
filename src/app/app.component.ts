60704e64d974af70ab7cd7b6e5e1e06d3ab592f1
Shubham Anand <60786471+22shubham22@users.noreply.github.com>
2022-06-03 19:58:21 +0530

    weather forecast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ShellbarUser, ShellbarUserMenu } from '@fundamental-ngx/core/shellbar';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  title = 'angular-starter';
-  a = 10;
-  b = 10;
-  constructor() {}
+export class AppComponent {
+  temp:any;
+  cloud:any;
+  hum:any;
+  wind:any;
+  name:any;
+  region:any;
+  country:any;
+  loading:boolean = false;
+  visible:boolean = false;
+  title:string = 'angular-starter';
 
-  ngOnInit() {
-    this.foo();
+  constructor(private http: HttpClient) {}
+  user: ShellbarUser = {
+    fullName: 'Shubham Anand',
+    colorAccent: 6
+  };
+
+  userMenu: ShellbarUserMenu[] = [
+    { text: 'Settings', callback: this.settingsCallback },
+    { text: 'Sign Out', callback: this.signOutCallback }
+  ];
+
+  settingsCallback(): void {
+    alert('Settings Clicked');
   }
 
-  foo() {
-    const a = 1;
-    const b = 5;
-    const c = a + b;
-    return 2 + 3;
+  signOutCallback(): void {
+    alert('Sign Out Clicked');
   }
+  submit(name: any){
+    this.loading=true;
+    this.visible=false;
+    var lat,long;
+    this.getLatLong(name).subscribe((data:any) => {
+        lat= data.data[0].latitude;
+        long= data.data[0].longitude;
+        this.getWeather(lat,long).subscribe((data:any) => {
+          this.loading=false;
+          this.temp=data.current.temperature;
+          this.cloud=data.current.cloudcover;
+          this.hum=data.current.humidity;
+          this.wind=data.current.wind_speed;
+          this.name=data.location.name;
+          this.region=data.location.region;
+          this.country=data.location.country;
+          this.visible=true;
+      });
+      });
+  }
+  getLatLong(name:any) {
+    let url = 'http://api.positionstack.com/v1/forward?access_key=a6af74dd0d1482a7002f06a794e3f9a8&query=';
+    url=url+name;
+    return this.http.get(url);
+  }
+  getWeather(lat:any,long:any){
+    const url = `http://api.weatherstack.com/current?access_key=2eb16bdead46872dc880b6bdc6f487da&query=${lat},${long}&units=m`;
+    return this.http.get(url);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDividerModule} from "@angular/material/divider";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {FormsModule} from "@angular/forms";
+import {
+  BusyIndicatorModule,
+  FacetModule,
+  FormLabelModule,
+  ObjectNumberModule,
+  ObjectStatusModule,
+  ShellbarModule,
+  TextModule
+} from "@fundamental-ngx/core";
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule, MatDividerModule, MatFormFieldModule, MatIconModule, FormsModule, ShellbarModule,
+    HttpClientModule, FacetModule, ObjectStatusModule, ObjectNumberModule, TextModule, FormLabelModule, BusyIndicatorModule],
   providers: [],
   bootstrap: [AppComponent],
 })
