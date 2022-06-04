9fa0bb2b5d00482f6c87c15500fd245b780128d8
Shubham Anand <60786471+22shubham22@users.noreply.github.com>
2022-06-04 11:52:40 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,6 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-starter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-starter');
+    expect(app.title).toEqual('angular-starte');
   });
 });
